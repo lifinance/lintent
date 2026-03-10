5bab87651465cd826bd363822d23a620b520c934
Alexander <git@lindgren.xyz>
2026-03-10 20:37:00 +0400

    Throw bad DBs for adding new tokens.

--- drizzle/0001_nifty_mephisto.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "fill_transactions" (
-	"id" text PRIMARY KEY NOT NULL,
-	"output_hash" text NOT NULL,
-	"tx_hash" text NOT NULL,
-	CONSTRAINT "fill_transactions_output_hash_unique" UNIQUE("output_hash")
-);
